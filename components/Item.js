956bab0673198f05a3da739915d251f880d5bcbc
denis-fominykh <fominykhdenis@gmail.com>
2020-07-19 20:09:14 +0300

    creating an item component

---
+++ components/Item.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { StyleSheet, View, Image, Text } from 'react-native';
+
+const Item = ({ item }) => {
+  return (
+    <View style={styles.itemContainer}>
+      <Image source={{ uri: item.strDrinkThumb }} style={styles.image} />
+      <Text style={styles.itemText}>{item.strDrink}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  itemContainer: {
+    width: '90%',
+    paddingHorizontal: '5%',
+    paddingVertical: 20,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  image: {
+    width: 100,
+    height: 100,
+  },
+  itemText: {
+    fontSize: 16,
+    paddingLeft: 21,
+  },
+});
+
+export default Item;
