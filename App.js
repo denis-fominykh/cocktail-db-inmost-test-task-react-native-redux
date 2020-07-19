ca0c0bdf4b5d1c34bffbd14dd1deaea7cd5906d3
denis-fominykh <fominykhdenis@gmail.com>
2020-07-19 20:00:50 +0300

    connecting redux store to the app

--- App.js
+++ App.js
@@ -3,22 +3,26 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import { SafeAreaProvider } from 'react-native-safe-area-context';
+import { Provider } from 'react-redux';
 
 import DrinksScreen from './screens/DrinksScreen';
 import FiltersScreen from './screens/FiltersScreen';
+import store from './redux/store';
 
 const Stack = createStackNavigator();
 
 const App = () => {
   return (
-    <SafeAreaProvider>
-      <NavigationContainer>
-        <Stack.Navigator initialRouteName="Drinks" headerMode="none">
-          <Stack.Screen name="Drinks" component={DrinksScreen} />
-          <Stack.Screen name="Filters" component={FiltersScreen} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </SafeAreaProvider>
+    <Provider store={store}>
+      <SafeAreaProvider>
+        <NavigationContainer>
+          <Stack.Navigator initialRouteName="Drinks" headerMode="none">
+            <Stack.Screen name="Drinks" component={DrinksScreen} />
+            <Stack.Screen name="Filters" component={FiltersScreen} />
+          </Stack.Navigator>
+        </NavigationContainer>
+      </SafeAreaProvider>
+    </Provider>
   );
 };
 
