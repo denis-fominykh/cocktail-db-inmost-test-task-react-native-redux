60485c8687decf0c0a2e3111a98ccaf61a13fcc2
denis-fominykh <fominykhdenis@gmail.com>
2020-07-19 20:07:26 +0300

    creating a new service to get categories

--- services/cocktaildbServices.js
+++ services/cocktaildbServices.js
@@ -1,14 +1,20 @@
 import axios from 'axios';
-import { getCocktails } from '../redux/actions';
+import { getCategories, getCocktails } from '../redux/actions';
 
 const _apiBase = 'https://www.thecocktaildb.com/api/json/v1/1';
 
-const getResourceItem = (param, key, value) => (dispatch) => {
+export const getItemsCategories = (param, key, value) => (dispatch) => {
   axios.get(`${_apiBase}/${param}.php?${key}=${value}`).then((response) => {
     if (response.status === 200) {
-      dispatch(getCocktails(response.data));
+      dispatch(getCategories(response.data.drinks));
     }
   });
 };
 
-export default getResourceItem;
+export const getResourceItem = (param, key, value) => (dispatch) => {
+  axios.get(`${_apiBase}/${param}.php?${key}=${value}`).then((response) => {
+    if (response.status === 200) {
+      dispatch(getCocktails(response.data.drinks));
+    }
+  });
+};
