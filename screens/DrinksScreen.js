6d8da5e857f8b4974c4dcaafa6307d02adec1628
denis-fominykh <fominykhdenis@gmail.com>
2020-07-19 20:12:16 +0300

    connecting state and actions from redux

--- screens/DrinksScreen.js
+++ screens/DrinksScreen.js
@@ -1,10 +1,40 @@
-import React from 'react';
-import { StyleSheet, Button } from 'react-native';
+import React, { useEffect } from 'react';
+import { StyleSheet, FlatList, Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { connect } from 'react-redux';
 
 import Header from '../components/Header';
+import Item from '../components/Item';
+import {
+  getItemsCategories,
+  getResourceItem,
+} from '../services/cocktaildbServices';
 
-const DrinksScreen = ({ navigation }) => {
+const DrinksScreen = ({
+  navigation,
+  drinks,
+  categories,
+  getItemsCategories,
+  getResourceItem,
+}) => {
+  const renderItem = ({ item }) => {
+    return <Item item={item} />;
+  };
+
+  useEffect(() => {
+    getResourceItem('filter', 'c', 'Ordinary Drink');
+  }, [drinks]);
+
+  const content = drinks ? (
+    <FlatList
+      data={drinks}
+      keyExtractor={(item) => item.idDrink}
+      renderItem={renderItem}
+    />
+  ) : (
+    <Text>Something went wrong!</Text>
+  );
+
   return (
     <SafeAreaView style={styles.container}>
       <Header
@@ -12,8 +42,25 @@ const DrinksScreen = ({ navigation }) => {
         type="filter"
         onPress={() => navigation.navigate('Filters')}
       />
+      {content}
     </SafeAreaView>
   );
+};
+
+const mapStateToProps = (state) => {
+  return {
+    drinks: state.drinks,
+    categories: state.categories,
+  };
+};
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    getItemsCategories: (param, key, value) =>
+      dispatch(getItemsCategories(param, key, value)),
+    getResourceItem: (param, key, value) =>
+      dispatch(getResourceItem(param, key, value)),
+  };
 };
 
 const styles = StyleSheet.create({
@@ -23,4 +70,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default DrinksScreen;
+export default connect(mapStateToProps, mapDispatchToProps)(DrinksScreen);
